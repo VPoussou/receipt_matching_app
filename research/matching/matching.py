7cab1c8c8fc498a1c4df0563e88e8254bbabf6c8
Lazare-Nogrette <lazare.nogrette@gmail.com>
2025-04-03 13:55:22 +0200

    updated matching function and removal of all csvs

--- research/matching/matching.py
+++ research/matching/matching.py
@@ -1,8 +1,13 @@
-from stringmatch import Match
 import pandas as pd
 import os
 import numpy as np
 from dateutil import parser
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# On oublie ces lignes là, il faut juste fournir un csv en entrée à la place et le convertir en dataframe
+
+r = """
 
 path_to_csv_folder = "bank_statements\\"
 
@@ -18,79 +23,99 @@ for filename in os.listdir(path_to_csv_folder):
 whole_df = pd.concat(df_list)
 
 whole_df.reset_index(drop=True, inplace=True)
+"""
 
+def get_best_match_with_transformer(query, candidates, model):
+    """
+    Retourne l'élément de la liste `candidates` le plus similaire à `query`
+    """
+    embeddings = model.encode([query] + candidates)
+    similarities = cosine_similarity([embeddings[0]], embeddings[1:])[0]
+    best_idx = similarities.argmax()
+    return candidates[best_idx], similarities[best_idx]
 
+def data_matching(source_csv, ocr_csv):
 
-#Pre-traitement des données d'entrée
-
-path_to_export = r"export.csv"
-
-ocr_output = pd.read_csv(path_to_export)
-
-def parse_date_safely(date_str):
-    # Ne rien faire si la valeur est vide ou autre chose qu'une string
-    if pd.isna(date_str) or not isinstance(date_str, str):
-        return date_str
-    try:
-        # parsing de la date
-        dt = parser.parse(date_str, fuzzy=True, dayfirst=False)
-        return dt.strftime('%Y-%m-%d')
-    # Retourne la valeur inchangée si échec
-    except Exception:
-        return date_str
-
-# Application sur la colonne
-ocr_output['date_of_purchase'] = ocr_output['date_of_purchase'].apply(parse_date_safely)
-
-ocr_output['vendor'] = ocr_output['name_of_store'].astype(str) + ' ' + (ocr_output['address'].astype(str))
-
-ocr_output['filename'] = ocr_output['filename'].str.replace(r'^.*\\', '', regex=True)
-
-ocr_output.to_csv("export_fixed.csv", sep=",", index=False)
-
-
-
-# Par ordre de hiérarchie: prix > date > vendeur > currency, on vérifie dans cette ordre
-# A chaque fois, on retient les lignes qui correspondent ou qui matchent le mieux au vendeur, et on attribue la facture correspondante
-# Après attribution de la facture, on marque la ligne comme checked, pour ne pas la reparcourir dans les itérations successives
-
-# Initialisation de l'instance qui fait le matching
-match = Match()
-
-# Les colonnes rajoutées pour assigner l'image et pour éliminer les lignes assignées des futures itérations
-whole_df['checked'] = False
-whole_df['assigned_picture'] = np.nan
-
-unassigned_pictures_list = []
-
-for index, _ in ocr_output.iterrows():
-    # Récupération des informations du dataframe output de l'OCR
-    vendor_entry = ocr_output.loc[index, 'vendor']
-    amount_entry = ocr_output.loc[index, 'total_price']
-    date_entry = ocr_output.loc[index, 'date_of_purchase']
-    picture_entry = ocr_output.loc[index, 'filename']
-
-    # Création d'un dataframe qui match les infos entre l'output et celui du relevé bancaire
-    sorted_df = whole_df[whole_df['checked'] == False]
-    sorted_df = whole_df[whole_df['amount'] == amount_entry]
-    sorted_df = whole_df[whole_df['date'] == (date_entry)]
+    #Pre-traitement des données OCR d'entrée
+    # Pareil, changer le chemin en entrée selon ce que fournit l'OCR
     
-    if not sorted_df.empty:
-        # Matching du nom du vendeur, on retient le meilleur et on l'assigne à la ligne dans le dataframe du relevé bancaire selon l'index
-        vendor_list = sorted_df['vendor'].values.tolist()
-        if (isinstance(vendor_list[0], str)):
-            best_matching = match.get_best_matches(str(vendor_entry), vendor_list, limit=1)
-            if len(best_matching) > 0:
-                best_matching_index = whole_df[whole_df['vendor'] == best_matching[0]].index[0]
-        else:
-            best_matching_index = whole_df[whole_df['date'] == (date_entry)].index[0]
-            
-        whole_df.loc[best_matching_index, 'checked'] = True
-        whole_df.loc[best_matching_index, 'assigned_picture'] = picture_entry
+    ocr_output = pd.read_csv(ocr_csv)
+    whole_df = pd.read_csv(source_csv)
+    
+    def parse_date_safely(date_str):
+        # Ne rien faire si la valeur est vide ou autre chose qu'une string
+        if pd.isna(date_str) or not isinstance(date_str, str):
+            return date_str
+        try:
+            # parsing de la date
+            dt = parser.parse(date_str, fuzzy=True, dayfirst=False)
+            return dt.strftime('%Y-%m-%d')
+        # Retourne la valeur inchangée si échec
+        except Exception:
+            return date_str
+    
+    # Application sur la colonne
+    ocr_output['date_of_purchase'] = ocr_output['date_of_purchase'].apply(parse_date_safely)
+    
+    ocr_output['vendor'] = ocr_output['name_of_store'].astype(str) + ' ' + (ocr_output['address'].astype(str))
+    
+    ocr_output['filename'] = ocr_output['filename'].str.replace(r'^.*\\', '', regex=True)
+    
+    #ocr_output.to_csv("export_fixed.csv", sep=",", index=False)
+    
+    
+    
+    # Par ordre de hiérarchie: prix > date > vendeur > currency, on vérifie dans cette ordre
+    # A chaque fois, on retient les lignes qui correspondent ou qui matchent le mieux au vendeur, et on attribue la facture correspondante
+    # Après attribution de la facture, on marque la ligne comme checked, pour ne pas la reparcourir dans les itérations successives
+    
+    # Initialisation de l'instance qui fait le matching
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    
+    # Les colonnes rajoutées pour assigner l'image et pour éliminer les lignes assignées des futures itérations
+    whole_df['checked'] = False
+    whole_df['assigned_picture'] = ''
+    
+    # Start matching
+    for index, row in ocr_output.iterrows():
+        if not isinstance(row['total_price'], (int, float)):
+            continue
+    
+        # Création d'un dataframe qui match les infos entre l'output et celui du relevé bancaire 
+        # Pour chaque attribut, s'il n'y a qu'un match trouvé, on l'assigne immédiatement et on passe à la prochaine itération
+        # On ne check que les lignes qui n'ont pas déjà une image assignée
+        filtered_df = whole_df[(whole_df['assigned_picture'] == '') & 
+                               (whole_df['amount'] == row['total_price'])]
 
-    else:
-        # Si on ne trouve pas de ligne correspondante, on garde l'image ici (un index tant que les données source de l'ocr ne sont pas dispo)
-        unassigned_pictures_list.append(picture_entry)
-
-
-whole_df.to_csv("current_output.csv")
+        # S'il n'y a qu'un match dès le check du prix, pas besoin de continuer, on établit d'emblée le matching
+        # Bonus: ne pas associer immédiatement l'image selon le prix, même s'il n'y a qu'un seul record
+        if filtered_df.shape[0] == 1:
+            match_index = filtered_df.index[0]
+            whole_df.loc[match_index, 'checked'] = True
+            whole_df.loc[match_index, 'assigned_picture'] = row['filename']
+            continue
+    
+        # On check la date, de manière rigide
+        filtered_df = filtered_df[filtered_df['date'] == row['date_of_purchase']]
+    
+        if filtered_df.shape[0] == 1:
+            match_index = filtered_df.index[0]
+            whole_df.loc[match_index, 'checked'] = True
+            whole_df.loc[match_index, 'assigned_picture'] = row['filename']
+            continue
+    
+        # On check le nom du vendeur, en retenant le plus similaire
+        if not filtered_df.empty:
+            # Matching du nom du vendeur, on retient le meilleur et on l'assigne à la ligne dans le dataframe du relevé bancaire selon l'index
+            candidate_vendors = filtered_df['vendor'].astype(str).tolist()
+            best_vendor, score = get_best_match_with_transformer(row['vendor'], candidate_vendors, model=model)
+            best_index = filtered_df[filtered_df['vendor'] == best_vendor].index[0]
+            whole_df.loc[best_index, 'checked'] = True
+            whole_df.loc[best_index, 'assigned_picture'] = row['filename']
+    
+    # On retient les images qui n'ont pas trouvé de match pour les montrer à l'utilisateur
+    picture_list = ocr_output['filename'].tolist()
+    missing_pictures = list(set(picture_list) - set(whole_df['assigned_picture'].dropna()))
+    
+    # Ici, on renvoie le dataframe
+    return whole_df
