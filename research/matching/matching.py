0d3fcccb37163ec2966c03390d3481266cce3d7e
VPoussou <v_poussou@hetic.eu>
2025-04-06 16:36:12 +0200

    Update matching.py

--- research/matching/matching.py
+++ research/matching/matching.py
@@ -75,7 +75,7 @@ def data_matching(source_csv, ocr_df):
     # Après attribution de la facture, on marque la ligne comme checked, pour ne pas la reparcourir dans les itérations successives
     
     # Initialisation de l'instance qui fait le matching
-    model = SentenceTransformer("all-MiniLM-L6-v2")
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
     
     # Les colonnes rajoutées pour assigner l'image et pour éliminer les lignes assignées des futures itérations
     whole_df['checked'] = False
